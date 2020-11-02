77a6f2c6f4658964c990b9417b9e5e71d71d87a5
Sergey Startsev <sergey@webstartsev.ru>
2020-11-02 14:36:18 +0300

    add eslint & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import React from "react";
-import cn from "classnames";
+import React from 'react';
+import cn from 'classnames';
 
-import s from "./App.module.scss";
+import s from './App.module.scss';
 
 const App: React.FunctionComponent = (): React.ReactElement => {
-    return (
-        <div className={cn(s.header, "color")}>
-            This is App component!
-        </div>
-    );
+  return <div className={cn(s.header, 'color')}>This is App component!</div>;
 };
 
 export default App;
