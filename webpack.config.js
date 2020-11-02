6f462dceb95cfe82d73b06ab1d2ef4b52ab5212e
Sergey Startsev <sergey@webstartsev.ru>
2020-11-02 15:11:30 +0300

    add file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,15 @@ module.exports = {
                     },
                     'sass-loader'
                 ]
+            },
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'images/[hash]-[name].[ext]'
+                    }
+                }
             }
         ]
     },
