b632fddca89ea4eefeaca2f62720a0c18387de9d
Sergey Startsev <sergey@webstartsev.ru>
2020-11-08 19:16:02 +0300

    update Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,14 +2,28 @@ import React from 'react';
 
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import Layout from '../../components/Layout';
 
 import s from './Home.module.scss';
+import Button from '../../components/Button';
+import Parallax from '../../components/Parallax';
 
 const HomePage = () => {
   return (
     <div className={s.root}>
       <Header />
-      <div className={s.content}>This is App component!</div>
+      <Layout className={s.contentWrap}>
+        <div className={s.contentText}>
+          <h1 className={s.contentTitle}>
+            <b>Find</b> all your favorite <b>Pokemon</b>
+          </h1>
+          <p>You can know the type of Pokemon, its strengths, disadvantages and abilities</p>
+          <Button onClick={() => {}}>See pokemons</Button>
+        </div>
+        <div className={s.contentParallax}>
+          <Parallax />
+        </div>
+      </Layout>
       <Footer />
     </div>
   );
