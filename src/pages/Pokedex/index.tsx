6e89e1b188d2cd6fc873672ee090503c26736f2e
Sergey Startsev <sergey@webstartsev.ru>
2020-11-10 11:03:47 +0300

    show pokemon list

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-
-import s from './Pokedex.module.scss';
 import Layout from '../../components/Layout';
 import Heading from '../../components/Heading';
+import Pokemon from '../../components/Pokemon';
+
+import POKEMONS from '../../mock/pokemon';
+
+import s from './Pokedex.module.scss';
 
 const PokedexPage = () => {
   return (
@@ -15,6 +18,20 @@ const PokedexPage = () => {
         <Heading tag="h1" className={s.contentTitle}>
           800 <b>Pokemons</b> for you to choose your favorite
         </Heading>
+
+        <div className={s.pokemonList}>
+          {POKEMONS.map((pokemon) => {
+            return (
+              <Pokemon
+                name={pokemon.name}
+                attack={pokemon.stats.attack}
+                defense={pokemon.stats.defense}
+                types={pokemon.types}
+                img={pokemon.img}
+              />
+            );
+          })}
+        </div>
       </Layout>
       <Footer />
     </div>
